3613582b98d42fbeab1a42df6acd5b74bdbbe7ee
Irakoze Armel <iraarmel90@gmail.com>
2026-05-05 23:51:34 +0200

    Changes in the Header

---
+++ script.js
@@ -0,0 +1,6 @@
+let elementId = document.getElementById("products");
+/* elementId.innerHTML = "Products section is under construction. Please check back later for updates."; */
+let hpLaptop = document.getElementById("laptopHpPavilion");
+hpLaptop.addEventListener("click", function() {
+    alert("You clicked on the HP Pavilion laptop. For more information, please contact us via WhatsApp.");
+});
